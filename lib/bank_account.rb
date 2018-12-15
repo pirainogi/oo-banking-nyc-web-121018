3e1cf7f26535115d01507aee114b738d1342d94f
pirainogi <pirainogi@gmail.com>
2018-12-15 12:21:53 -0500

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,45 @@
+require 'pry'
 class BankAccount
 
+  attr_reader :name
+  attr_accessor :balance, :status, :sender, :receiver
+  #initialize a Bank Account
+  #initialize with a name argument
+  #always initializes with a balance of 1000
+  #always initializes iwth a status of "open"
+  #can't change its name
+  def initialize(name)
+    @name = name #string
+    @balance = 1000 #integer
+    @status = "open" #string
+  end
+
+  #can deposit money into its acct
+  #add deposit amt to current balance?
+  def deposit(amount)
+    @balance += amount
+    @balance
+  end
+
+  #return balance
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  #valid = open status and balance > 0
+  def valid?
+    if @status == "open" && @balance > 0
+      return true
+    else
+      return false
+    end
+  end
+
+  #can close its account -- clear all?
+  def close_account
+    @status = "closed"
+  end
+
+
 end
+#binding.pry
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,51 @@
+require 'pry'
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender #BankAccount instance
+    @receiver = receiver #BankAccount instance
+    @amount = amount #amount sending #integer
+    @status = "pending"
+  end
+
+  #can check that both accts are valid
+  #calls on sender and rcvrs valid? methods
+  def valid?
+    @sender.valid? == true && @receiver.valid? == true
+  end
+
+  #execute a successful xfer btw accts
+  #each xfer can only happen once (makes an instance of an xfer?)
+  #rejects xfer if sender =/= valid acct
+  def execute_transaction
+    if valid? && @sender.balance > amount && @status == "pending"
+    @sender.balance -= amount
+    @receiver.balance += amount
+    @status = "complete"
+    else
+      reject
+    end
+  end
+
+  def reject
+    @status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+
+
+
+  #can reverse xfer btw two accts
+  #can only reverse executed xfers
+  def reverse_transfer
+    if @status == "complete"
+    @sender.balance += amount
+    @receiver.balance -= amount
+    @status = "reversed"
+    end
+  end
+
+
 end
+#binding.pry
